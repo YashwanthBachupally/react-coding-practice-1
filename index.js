1358a10854785d1edbfa1b7bb11bfea982e9c887
yashwanth <bachupally.yashwanth@gmail.com>
2022-01-10 16:59:45 +0000

    i don't get get what it is

---
+++ index.js
@@ -0,0 +1,18 @@
+const element = (
+  //   Write your code here.
+  <div className="bg-cont">
+    <h1 className="title">Super Over League</h1>
+    <div className="img-cont">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        className="img"
+      ></img>
+      <img
+        className="img"
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+      ></img>
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
